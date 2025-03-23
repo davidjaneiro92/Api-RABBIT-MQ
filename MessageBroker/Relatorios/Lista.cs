800cd224902b22da825433d8bb98c27e9d3c388e
davidfavu92@gmail.com <davidfavu92@gmail.com>
2025-03-22 21:17:34 -0300

    comesando cria a configuracoes

---
+++ MessageBroker/Relatorios/Lista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBroker.Relatorios
+{
+    public class Lista
+    {
+        public List<SolicitacaoRelatorio> Relatorio { get; set; }
+    }
+
+    public class SolicitacaoRelatorio
+    { 
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
+        public DateTime? ProcessedTime { get; set; }
+    }
+}
